246aac24be485e4b88b35db2151e870bd5853048
kazungu61 <kazungu61@gmail.com>
2018-12-03 12:44:42 +0300

    variable change

--- mpesa/b2c.php
+++ mpesa/b2c.php
@@ -24,7 +24,7 @@
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($curl, CURLOPT_HEADER, FALSE);
-  curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
+  curl_setopt($curl, CURLOPT_USERPWD, $consumerkey.':'.$consumersecret);
   $result = curl_exec($curl);
   $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
   $result = json_decode($result);
